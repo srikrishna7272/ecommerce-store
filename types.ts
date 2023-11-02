44121d5053ea53cfda0b86f5cba80fc4a242ddcf
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-11-01 22:04:33 -0700

    built ecommerce store for admin.

--- types.ts
+++ types.ts
@@ -6,8 +6,8 @@ export interface Product {
   isFeatured: boolean;
   size: Size;
   color: Color;
-  images: Image[]
-};
+  images: Image[];
+}
 
 export interface Image {
   id: string;
@@ -18,22 +18,22 @@ export interface Billboard {
   id: string;
   label: string;
   imageUrl: string;
-};
+}
 
 export interface Category {
   id: string;
   name: string;
   billboard: Billboard;
-};
+}
 
 export interface Size {
   id: string;
   name: string;
   value: string;
-};
+}
 
 export interface Color {
   id: string;
   name: string;
   value: string;
-};
+}
